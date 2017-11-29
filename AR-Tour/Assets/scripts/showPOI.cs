5fdf11b94c6b0bf987af0003996fd9f9f1a7471e
Matt <mccormackm14@gmail.com>
2017-11-29 10:42:38 -0800

    clickable poi marker

---
+++ AR-Tour/Assets/scripts/showPOI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showPOI : MonoBehaviour {
+
+    public GameObject cubeshow;
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("TEST");
+            cubeshow.SetActive(true);
+          
+        }
+    }
+
+
+
+}
