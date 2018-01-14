eb888e452ca98ab38fbd88c3a0bf9fa399835295
Ashley Bernhardt <ashleybernhardt@hotmail.com>
2018-01-14 14:36:07 -0800

    working on button animations

---
+++ AR-Tour/Assets/scripts/switchPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchPanel : MonoBehaviour
+{
+
+    public GameObject infoPanel;
+    public GameObject picturePanel;
+    public GameObject videoPanel; 
+
+    public void activateInfo()
+    {
+        infoPanel.SetActive(true);
+    }
+
+    public void closeInfo()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    public void activatePicture()
+    {
+        picturePanel.SetActive(true); 
+    }
+
+    public void closePictrue()
+    {
+        picturePanel.SetActive(false);
+    }
+
+    public void activateVideo()
+    {
+        videoPanel.SetActive(true);
+    }
+
+    public void closeVideo()
+    {
+        videoPanel.SetActive(false);
+    }
+}
