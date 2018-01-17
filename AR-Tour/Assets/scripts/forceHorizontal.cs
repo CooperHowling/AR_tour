fced400b238d723edd7d3b43a2ba75c2f8c9f729
Cooper <cooper.howling@gmail.com>
2018-01-17 14:22:52 -0800

    changed build settings and auto rotate

--- AR-Tour/Assets/scripts/forceHorizontal.cs
+++ AR-Tour/Assets/scripts/forceHorizontal.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class forceHorizontal : MonoBehaviour {
+public class ForceHorizontal : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
