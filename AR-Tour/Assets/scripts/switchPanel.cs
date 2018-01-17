2040cec7a89616ff7ba09c95c8cb58fbd22b4ba4
Cooper <cooper.howling@gmail.com>
2018-01-16 20:11:49 -0800

    merged dogAR with master

---
+++ AR-Tour/Assets/scripts/switchPanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchPanel : MonoBehaviour
+{
+
+    public GameObject infoPanel;
+    public GameObject picturePanel;
+    public GameObject videoPanel; 
+
+    public void activateInfo()
+    {
+        infoPanel.SetActive(true);
+        picturePanel.SetActive(false);
+        videoPanel.SetActive(false);
+    }
+
+    public void closeInfo()
+    {
+        infoPanel.SetActive(false);
+        
+    }
+
+    public void activatePicture()
+    {
+        picturePanel.SetActive(true);
+        infoPanel.SetActive(false);
+        videoPanel.SetActive(false);
+        
+    }
+
+    public void closePictrue()
+    {
+        picturePanel.SetActive(false);
+    }
+
+    public void activateVideo()
+    {
+        videoPanel.SetActive(true);
+        picturePanel.SetActive(false);
+        infoPanel.SetActive(false);
+    }
+
+    public void closeVideo()
+    {
+        videoPanel.SetActive(false);
+    }
+}
