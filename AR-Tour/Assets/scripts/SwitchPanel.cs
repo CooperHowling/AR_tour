f6571f7c30cda666ab36af973d6fb2d0a86fb717
Cooper <cooper.howling@gmail.com>
2018-01-17 14:39:43 -0800

    bug fixes, rotation and bio AR

---
+++ AR-Tour/Assets/scripts/SwitchPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SwitchPanel : MonoBehaviour
+{
+
+    public GameObject infoPanel;
+    public GameObject picPanel;
+    public GameObject vidPanel;
+
+    public void openInfo()
+    {
+        infoPanel.SetActive(true);
+        picPanel.SetActive(false);
+        vidPanel.SetActive(false);
+    }
+
+    public void openPic()
+    {
+        infoPanel.SetActive(false);
+        picPanel.SetActive(true);
+        vidPanel.SetActive(false);
+    }
+
+    public void openVid()
+    {
+        infoPanel.SetActive(false);
+        picPanel.SetActive(false);
+        vidPanel.SetActive(true);
+    }
+}
