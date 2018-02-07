b1dcfd0646e59c070047a1ddf694e0b6c93dbb0e
BlueSteel <alexandershaw28@gmail.com>
2018-02-06 20:59:27 -0800

    Adjusted the Avatar circle and created a loading scene

---
+++ AR-Tour/Assets/scripts/LoadTargetSceneButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTargetSceneButton : MonoBehaviour {
+
+
+    public void LoadSceneName(string name)
+    {
+
+        LoadingScreenManager.LoadScene(name);
+    }
+
+
+}
