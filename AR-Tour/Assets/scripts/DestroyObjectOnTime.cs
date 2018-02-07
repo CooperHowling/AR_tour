593bb21500e39f6ed65b69c907182bd96103ae58
BlueSteel <alexandershaw28@gmail.com>
2018-02-06 22:06:37 -0800

    Loading screen works and Compass is reoriented

---
+++ AR-Tour/Assets/scripts/DestroyObjectOnTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectOnTime : MonoBehaviour {
+    public GameObject Loader;
+    public float timer;
+
+	void Start () {
+
+        Destroy(Loader, timer);
+
+	}
+	
+
+}
---
+++ AR-Tour/Assets/scripts/LoadSceneByName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneByName : MonoBehaviour {
+
+
+    public void LoadSceneName(string name)
+    {
+
+        LoadingScreenManager.LoadScene(name); 
+    }
+
+
+}
