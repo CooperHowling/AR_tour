b0a4dd2418a92565cd5071728a4215c190559839
Ashley Bernhardt <ashleybernhardt@hotmail.com>
2018-02-16 12:39:51 -0800

    rotating box script/ box for comp sci game

---
+++ AR-Tour/Assets/scripts/compSciRotateBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compSciRotateBox : MonoBehaviour
+{
+
+    float timeCounter = 0;
+    float speed, radius;
+
+    void Start()
+    {
+        speed = 1;
+        radius = 200;
+    }
+
+    void Update()
+    {
+        timeCounter += Time.deltaTime * speed;
+
+        float x = Mathf.Sin(timeCounter) * radius;
+        float y = -57;
+        float z = Mathf.Cos(timeCounter) * radius;
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
