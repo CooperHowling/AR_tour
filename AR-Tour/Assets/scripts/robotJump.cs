f05c8f86d2fa56adaa04f5d24755b57d1fd6ba65
Ashley Bernhardt <ashleybernhardt@hotmail.com>
2018-02-16 14:12:30 -0800

    Robot jumping

---
+++ AR-Tour/Assets/scripts/robotJump.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotJump : MonoBehaviour
+{
+
+    
+    float force = 20;
+    public Rigidbody rb;
+
+
+    
+
+    // Update is called once per frame
+    public void OnMouseDown()
+    {
+        rb.AddForce(rb.transform.up * force, ForceMode.Impulse);
+    }
+}
