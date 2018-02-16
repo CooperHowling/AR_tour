8f40c993e92c65ca6a623eedd738f5a40b24145a
Ashley Bernhardt <ashleybernhardt@hotmail.com>
2018-02-16 14:54:12 -0800

    robot jumps, only able to click button once robot is at bottom of arc

--- AR-Tour/Assets/scripts/robotJump.cs
+++ AR-Tour/Assets/scripts/robotJump.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class robotJump : MonoBehaviour
 {
 
-    
-    float force = 20;
+
+    float force = 50;
     public Rigidbody rb;
+    float pose;
 
 
-    
+
+    void Start()
+    {
+        pose = rb.position.y;
+        pose = Mathf.Round(pose);
+    }
 
     // Update is called once per frame
     public void OnMouseDown()
     {
-        rb.AddForce(rb.transform.up * force, ForceMode.Impulse);
+        float currentPose = Mathf.Round(rb.position.y);
+       
+       if (currentPose== (pose))
+        {
+            //rb.velocity = 20;
+            
+            rb.AddForce(rb.transform.up * force, ForceMode.Impulse);
+       }
     }
 }
