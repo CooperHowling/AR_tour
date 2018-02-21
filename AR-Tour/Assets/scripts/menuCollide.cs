c48570f220206917e3235ef0935ca18b8c3f4a35
Matt <mccormackm14@gmail.com>
2018-02-20 23:02:01 -0800

    Menu detection with player radius

---
+++ AR-Tour/Assets/scripts/menuCollide.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuCollide : MonoBehaviour {
+
+    public GameObject historyMenu;
+    public GameObject uncMenu;
+    public GameObject eescMenu;
+    public GameObject ecologyMenu;
+    public GameObject philMenu;
+    public GameObject econMenu;
+
+    void OnCollision(Collision col)
+    {
+
+        if (col.gameObject.name == "unc")
+        {
+            uncMenu.SetActive(true);
+
+            historyMenu.SetActive(false);
+            eescMenu.SetActive(false);
+            ecologyMenu.SetActive(false);
+            philMenu.SetActive(false);
+            econMenu.SetActive(false);
+        }
+
+        if (col.gameObject.name == "history")
+        {
+            historyMenu.SetActive(true);
+
+            uncMenu.SetActive(false);
+            eescMenu.SetActive(false);
+            ecologyMenu.SetActive(false);
+            philMenu.SetActive(false);
+            econMenu.SetActive(false);
+
+        }
+        if (col.gameObject.name == "eesc")
+        {
+            eescMenu.SetActive(true);
+
+            historyMenu.SetActive(false);
+            uncMenu.SetActive(false);
+            ecologyMenu.SetActive(false);
+            philMenu.SetActive(false);
+            econMenu.SetActive(false);
+
+        }
+        if (col.gameObject.name == "ecology")
+        {
+            ecologyMenu.SetActive(true);
+            print(col.gameObject.name);
+
+            eescMenu.SetActive(false);
+            historyMenu.SetActive(false);
+            uncMenu.SetActive(false);
+            philMenu.SetActive(false);
+            econMenu.SetActive(false);
+
+        }
+        if (col.gameObject.name == "phil")
+        {
+            philMenu.SetActive(true);
+
+            ecologyMenu.SetActive(false);
+            eescMenu.SetActive(false);
+            historyMenu.SetActive(false);
+            uncMenu.SetActive(false);
+            econMenu.SetActive(false);
+
+        }
+        if (col.gameObject.name == "econ")
+        {
+            econMenu.SetActive(true);
+
+            philMenu.SetActive(false);
+            ecologyMenu.SetActive(false);
+            eescMenu.SetActive(false);
+            historyMenu.SetActive(false);
+            uncMenu.SetActive(false);
+
+        }
+    }
+
+
+
+}
