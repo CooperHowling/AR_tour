613f464e7041c0b22a03e85acb1c29090002dbb7
Cooper <cooper.howling@gmail.com>
2018-03-05 14:13:36 -0800

    removed folder at head, added alex's scripts to be implemented

---
+++ AR-Tour/Assets/scripts/ControlVideos.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlVideos: MonoBehaviour {
+
+    public GameObject CameraForVideo;
+    public GameObject MainCamera;
+    public GameObject VideoPlayer;
+
+    public static string YoutubeVideoCode;
+    public string code = YoutubeVideoCode;
+  
+    void Start()
+    {
+        MainCamera.SetActive(true);
+        CameraForVideo.SetActive(false);
+        VideoPlayer.SetActive(false);
+    }
+	
+    void Load2DVideo(string videocode)
+    {
+
+        YoutubeVideoCode = videocode;
+        MainCamera.SetActive(false);
+        CameraForVideo.SetActive(true);
+        VideoPlayer.SetActive(true);
+
+    }
+
+    void Load360Video(string videocode)
+    {
+        SceneManager.LoadScene("360VideoVlayer");
+        YoutubeVideoCode = videocode;
+    }
+
+    void Close2DVideo()
+    {
+        MainCamera.SetActive(true);
+        CameraForVideo.SetActive(false);
+        VideoPlayer.SetActive(false);
+    }
+}
---
+++ AR-Tour/Assets/scripts/GyroControlCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControlCamera : MonoBehaviour {
+
+    public GameObject Camera;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        Camera.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+    }
+
+
+}
