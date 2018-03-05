5f68fc6c4b9c77016175d652d66f8db3df222751
ashpash <ashleybernhardt@hotmail.com>
2018-03-05 13:46:51 -0800

    interacting with BARK characters

--- AR-Tour/Assets/scripts/DogAnimations.cs
+++ AR-Tour/Assets/scripts/DogAnimations.cs
@@ -2,19 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DogAnimations : MonoBehaviour {
+public class DogAnimations : MonoBehaviour
+{
 
-    public Animator dogAnim; 
+    public GameObject dog;
+    int girlC = 0;
+    int dogC = 0;
+    public Animator charAnim;
+    Animator dogAnim;
+ 
+    
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-        dogAnim = GetComponent<Animator>(); 
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        dogAnim = dog.GetComponent<Animator>(); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+        if (Input.GetMouseButton(0))
+        {
+            if(Physics.Raycast(ray,out hit))
+            {
+                if (hit.collider.name == "SportyGirl")
+                {
+                    Debug.Log("NAME: " + hit.collider.name);
+                    
+                    if (girlC % 3 == 0)
+                    {
+                        charAnim.Play("Fire");
+                        girlC++;
+                    }
+                    else if (girlC % 3 == 1)
+                    {
+                        charAnim.Play("Jump");
+                        girlC++;
+                    }
+                    else if (girlC % 3 == 2)
+                    {
+                        charAnim.Play("Walk");
+                        
+                    }
+                }
+                if (hit.collider.name == "Beagle")
+                {
+                    Debug.Log("HIT DOGGO");
+                   
+                    if (dogC % 3 == 0)
+                    {
+                        dogAnim.Play("BeagleIdleSniff");
+                        dogC++;
+                    }
+                    else if (dogC % 3 == 1)
+                    {
+                        dogAnim.Play("BeagleRun");
+                        dogC++;
+                    }
+                    else if (dogC % 3 == 2)
+                    {
+                        dogAnim.Play("BeagleIdleDig");
+                        dogC++;
+                    }
+                }
+                
+            }
+        }
+    }
 }
+
