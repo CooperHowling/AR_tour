42b7713ca6d8676562a8936f74bd9e2aad5a8eae
ashpash <ashleybernhardt@hotmail.com>
2018-03-05 10:19:10 -0800

    animations work

---
+++ AR-Tour/Assets/scripts/DogAnimations.cs
@@ -0,0 +1,20 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogAnimations : MonoBehaviour {
+
+    public Animator dogAnim; 
+
+	// Use this for initialization
+	void Start () {
+
+        dogAnim = GetComponent<Animator>(); 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
