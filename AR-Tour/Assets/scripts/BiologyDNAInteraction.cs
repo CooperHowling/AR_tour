8d79185fbec3398d7905f8b89036b7a6ca4435e2
Cooper <cooper.howling@gmail.com>
2018-03-07 12:37:56 -0800

    stashed changes

---
+++ AR-Tour/Assets/scripts/BiologyDNAInteraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiologyDNAInteraction : MonoBehaviour {
+
+    public GameObject anti;
+    public GameObject sense;
+    int EO = 0; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit(); 
+
+        if (Input.GetMouseButton(0))
+        {
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.name == "DNA"  && EO%2 == 0)
+                {
+                    anti.SetActive(true);
+                    sense.SetActive(false);
+                    EO++;
+                    Debug.Log(EO);
+                }
+
+                if (hit.collider.name == "DNA" && EO%2 == 1)
+                {
+                    anti.SetActive(false);
+                    sense.SetActive(true);
+                    EO++;
+                    Debug.Log(EO); 
+                }
+            }
+        }
+	}
+}
