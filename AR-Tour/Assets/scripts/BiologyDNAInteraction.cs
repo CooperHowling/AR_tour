97510ce52a724104bd63c1af1ede0b28bb743e0f
Ashley Bernhardt <ashleybernhardt@hotmail.com>
2018-03-20 22:51:06 -0700

    reinstated simpler DNA strand

--- AR-Tour/Assets/scripts/BiologyDNAInteraction.cs
+++ AR-Tour/Assets/scripts/BiologyDNAInteraction.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class BiologyDNAInteraction : MonoBehaviour {
 
+    public GameObject ladder;
     public GameObject anti;
     public GameObject sense;
-    int EO = 0; 
+    public GameObject red;
+    public GameObject blue; 
+    public GameObject a,b,c,d,e,f,g,h,i;
+
+    float speed = 10f;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,36 +19,8 @@ public class BiologyDNAInteraction : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit = new RaycastHit(); 
 
-        if (Input.GetMouseButton(0))
-        {
-            if(Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider.name == "DNA"  &&  sense.activeSelf == false && anti.activeSelf == false)
-                {
-                    anti.SetActive(true);
-                    sense.SetActive(false);
-                    EO++;
-                    Debug.Log("anti Active");
-                }
-
-                if (hit.collider.name == "DNA" && anti.activeSelf == true)
-                {
-                    anti.SetActive(false);
-                    sense.SetActive(true);
-                    EO++;
-                    Debug.Log("sense Active"); 
-                }
-
-                else
-                {
-                    anti.SetActive(false);
-                    sense.SetActive(false);
-                    Debug.Log("NOTHING ACTIVE"); 
-                }
-            }
-        }
+        transform.Rotate(Vector3.down, speed * Time.deltaTime);
+       
 	}
 }
