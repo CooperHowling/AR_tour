6343a838be0499a746645e391a0f902d8652f332
Matt <mccormackm14@gmail.com>
2018-03-29 03:49:26 -0700

    added legened / splash screen fix

--- AR-Tour/Assets/scripts/switchScene.cs
+++ AR-Tour/Assets/scripts/switchScene.cs
@@ -5,14 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class switchScene : MonoBehaviour {
 
-
-    
-
+	public GameObject legend;
 
     public void SwitchScene(string scene)
     {
         SceneManager.LoadScene(scene);
     }
+
+	public void openLegend() {
+
+		legend.SetActive (true);
+
+	}
+
+
+	public void closeLegend() {
+
+		legend.SetActive (false);
+
+	}
 
 
 }
